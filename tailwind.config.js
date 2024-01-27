f66f08c6b9a96e4263f58512652222f4f0fabd75
Arafin Anan <samsularafinanan@gmail.com>
2024-01-27 06:30:12 -0800

    first

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+          colors: {
+            customColorOrange: '#FF4240',
+            customColorBlack: '#131318',
+          },
+        },
+      },
+    plugins: [],
+}
